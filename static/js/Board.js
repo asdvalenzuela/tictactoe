e9d4eec39d2a567d7350cab244a20f3dfc2b883e
Alaina <asdvalenzuela@gmail.com>
2015-02-19 20:05:11 -0800

    more explicit variable names rows and columns

--- static/js/Board.js
+++ static/js/Board.js
@@ -2,12 +2,12 @@ function Board(blankSpaces,xSpaces,oSpaces) {
     this.blankSpaces = blankSpaces || ['a1','a2','a3','b1','b2','b3','c1','c2','c3'];
     this.xSpaces = xSpaces || [];
     this.oSpaces = oSpaces || [];
-    this.letters = ['a', 'b', 'c'];
-    this.numbers = ['1', '2', '3'];
+    this.rows_ = ['a', 'b', 'c'];
+    this.columns = ['1', '2', '3'];
     this.clearBoard = function() {
-        for (var i = 0; i < this.letters.length; i++) {
-            for (var j = 0; j < this.numbers.length; j++) {
-                spaceId = '#' + this.letters[i] + this.numbers[j];
+        for (var i = 0; i < this.rows_.length; i++) {
+            for (var j = 0; j < this.columns.length; j++) {
+                spaceId = '#' + this.rows_[i] + this.columns[j];
                 $(spaceId).html('').css('pointer-events', 'auto');
             }
         }
@@ -38,9 +38,9 @@ function Board(blankSpaces,xSpaces,oSpaces) {
         }
     };
     this.preventBoardChanges = function() {
-        for (var i = 0; i < this.letters.length; i++) {
-            for (var j = 0; j < this.numbers.length; j++) {
-                spaceId = '#' + this.letters[i] + this.numbers[j];
+        for (var i = 0; i < this.rows_.length; i++) {
+            for (var j = 0; j < this.columns.length; j++) {
+                spaceId = '#' + this.rows_[i] + this.columns[j];
                 $(spaceId).css('pointer-events', 'none');
             }
         }
